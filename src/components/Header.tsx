eaf3b52b4de20b3567b6ecac87e96c5c3a8dee84
labt1 <labt_100@hotmail.com>
2024-11-14 14:23:23 -0500

    pagina

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,7 +68,7 @@ export const Header = () => {
     }
 
     const t = useTranslations();
-    const { person, home, about, blog, work, gallery } = renderContent(t);
+    const { person, home, about, blog, work } = renderContent(t);
 
     return (
         <Flex style={{height: 'fit-content'}}
@@ -125,14 +125,6 @@ export const Header = () => {
                                 href={`/${params?.locale}/blog`}
                                 selected={pathname.startsWith('/blog')}>
                                 <Flex paddingX="2" hide="s">{blog.label}</Flex>
-                            </ToggleButton>
-                        )}
-                        { routes['/gallery'] && (
-                            <ToggleButton
-                                prefixIcon="gallery"
-                                href={`/${params?.locale}/gallery`}
-                                selected={pathname.startsWith('/gallery')}>
-                                <Flex paddingX="2" hide="s">{gallery.label}</Flex>
                             </ToggleButton>
                         )}
                     </Flex>
